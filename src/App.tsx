8d8bd7ad76e664ee1f306b9720cfad4f69c5b4c2
jamesdoth <jamesdoth@gmail.com>
2023-02-02 20:35:01 -0800

    small update

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,18 @@ function App() {
     <body className='min-h-screen bg-slate-800'>
       <div id='first-section' className='flex h-screen justify-center items-center'>
         <div className='flex flex-col'>
-          <p className='text-center text-gray-200 text-9xl mb-4'>Hello World</p>
+          <p className='text-center text-gray-200 text-7xl mb-4'>J i m &nbsp;&nbsp; H a n</p>
           <ScrollButton id='first-section-button' section='second-section' label='Click me' />
         </div>
       </div>
       <div id='second-section' className='flex h-screen justify-center items-center'>
         <div className='flex flex-col'>
-          <p className='text-center text-gray-200 text-9xl mb-4'>Hello World</p>
+          <p className='text-center text-gray-200 text-3xl mb-4'>About Me</p>
           <ScrollButton id='second-section-button' section='first-section' label='Click me' />
         </div>
       </div>
     </body>
   )
 }
-
-    // <body className='min-h-screen bg-slate-800'>
-    //   <div id='first-section' className='flex h-screen justify-center items-center'>
-    //     <div className='flex flex-col'>
-    //       <p className='text-center text-gray-200 text-9xl mb-4'>Hello World</p>
-    //       <button className="bg-blue-500 text-white px-4 py-2 rounded w-48 mx-auto" onClick={() => {
-    //         const secondSection = document.getElementById('second-section');
-    //         if (secondSection) {
-    //           secondSection.scrollIntoView({ behavior: 'smooth' });
-    //         }
-    //       }}>Click me</button>
-    //     </div>
-    //   </div>
-    //   <div id='second-section' className='flex h-screen justify-center items-center'>
-    //     <div className='flex flex-col'>
-    //       <p className='text-center text-gray-200 text-9xl mb-4'>Hello World</p>
-    //       <button className="bg-blue-500 text-white px-4 py-2 rounded w-48 mx-auto" onClick={() => {
-    //         const firstSection = document.getElementById('first-section');
-    //         if (firstSection) {
-    //           firstSection.scrollIntoView({ behavior: 'smooth' });
-    //         }
-    //       }}>Click me</button>
-    //     </div>
-    //   </div>
-    // </body>
 
 export default App
