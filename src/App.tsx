d52a096120b04da1af982906b1110dda53c00523
jamesdoth <jamesdoth@gmail.com>
2023-02-11 08:02:13 -0800

    removed absolute. resolved with mb

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <body className='min-h-screen bg-slate-800'>
       <div id='first-section' className='flex h-screen justify-center items-center'>
-        <div className='flex flex-col'>
-          <p className="text-center text-gray-200 text-7xl mb-44 tracking-3 z-10">Jim Han</p>
-          <div className="absolute inset-0 flex items-center justify-center">
+        <div className='flex flex-col mb-16'>
+          <p className="text-center text-gray-200 max-w-none text-7xl mb-12 tracking-3 z-10">Jim Han</p>
+          <div className="flex items-center justify-center">
             <ScrollButton id='first-section-button' section='second-section' label='Click me' />
           </div>
         </div>
