eb30308e9e683c80fc3df35744c3befecfdcd6f4
jamesdoth <jamesdoth@gmail.com>
2023-02-15 13:52:09 -0800

    framer onclick

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,21 +2,36 @@ import { motion } from 'framer-motion';
 import { useState } from 'react';
 
 function Card() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCardClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleExpandedCardClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+  };
+
   return (
     <div className='bg-slate-500'>
-      <motion.div className='flex flex-col justify-center items-center'>
+      <motion.div
+        onClick={handleCardClick}
+        className='flex flex-col justify-center items-center p-4'
+      >
         <motion.h2 className='text-white'>Hello World</motion.h2>
-        <motion.div>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet
-            repudiandae voluptas commodi hic nisi neque inventore natus. Saepe,
-            incidunt tempora?
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi,
-            temporibus.
-          </p>
-        </motion.div>
+        {isOpen && (
+          <motion.div className='w-96' onClick={handleExpandedCardClick}>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet
+              repudiandae voluptas commodi hic nisi neque inventore natus.
+              Saepe, incidunt tempora?
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Excepturi, temporibus.
+            </p>
+          </motion.div>
+        )}
       </motion.div>
     </div>
   );
