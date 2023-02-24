363e1ea0b7e3827b8b8781d8ea12018003b4de42
jamesdoth <jamesdoth@gmail.com>
2023-02-24 07:49:04 -0800

    figured out button margin

--- src/content/Survey/Survey.tsx
+++ src/content/Survey/Survey.tsx
@@ -13,25 +13,28 @@ export function Survey() {
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering.
         </p>
-        <div className='flex justify-between'>
-          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200 mx-3'>
+        {/* mx-3 was max for individual buttons. mx inside div was the correct way to move buttons closer together */}
+        <div className='flex justify-between mx-8'>
+          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200'>
             1
           </button>
-          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200 mx-3'>
+          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200'>
             2
           </button>
-          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200 mx-3'>
+          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200'>
             3
           </button>
-          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200 mx-3'>
+          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200'>
             4
           </button>
-          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200 mx-3'>
+          <button className='bg-slate-800 rounded-full w-9 h-9 text-slate-200'>
             5
           </button>
         </div>
         <div className='flex justify-center'>
-          <button className='border rounded text-slate-200 mt-6'>Submit</button>
+          <button className='border rounded-xl w-16 h-8 text-slate-200 mt-6'>
+            Submit
+          </button>
         </div>
       </div>
     </div>
