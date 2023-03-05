43d264f33058b148a5334d81501ee57d2b2287da
jamesdoth <jamesdoth@gmail.com>
2023-03-05 07:51:04 -0800

    todolist funcs

--- src/content/TodoList/ToDoList.tsx
+++ src/content/TodoList/ToDoList.tsx
@@ -5,3 +5,25 @@ interface TodoItem {
   title: string;
   id: number;
 }
+
+export const TodoList = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+
+  const handleAddTodo = () => {
+    if (!inputValue) return;
+
+    const newTodo: TodoItem = {
+      title: inputValue,
+      id: Date.now(),
+    };
+
+    setTodos([...todos, newTodo]);
+    setInputValue('');
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+};
