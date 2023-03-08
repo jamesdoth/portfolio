58b16f9835e95bb07ff7a03bbd4cdbe618794112
jamesdoth <jamesdoth@gmail.com>
2023-03-08 09:13:36 -0800

    impl interface/finally building comp

--- src/content/Weather/Weather.tsx
+++ src/content/Weather/Weather.tsx
@@ -1,28 +1,37 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+  };
+}
+
 const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
-const WeatherApp = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=${apiKey}`
-        );
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+// const WeatherApp = () => {
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get(
+//           `https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=${apiKey}`
+//         );
+//         console.log(response.data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchData();
+//   }, []);
 
-  return (
-    <div>
-      <h1>Weather App</h1>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Weather App</h1>
+//     </div>
+//   );
+// };
 
-export default WeatherApp;
+// export default WeatherApp;
