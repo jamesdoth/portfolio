a10ce95ac8df7d0a2ae555310031ae5c758e29d9
jamesdoth <jamesdoth@gmail.com>
2023-03-10 10:25:53 -0800

    cardholder name

--- src/content/CardForm/CardForm.tsx
+++ src/content/CardForm/CardForm.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 const CardForm = () => {
+  const [cardholderName, setCardholderName] = useState('');
+
+  const handleCardholderNameChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCardholderName(e.target.value);
+  };
+
   return (
-    <div>
-      <h1>Card Form</h1>
+    <div className='max-w-md mx-auto mt-8'>
+      <div className='mb-4'>
+        <label className='block font-medium mb-1'>CARDHOLDER NAME</label>
+        <input
+          type='text'
+          placeholder='e.g. Jane Appleseed'
+          className='w-full p-2 border-2 border-gray-200 rounded'
+          value={cardholderName}
+          onChange={handleCardholderNameChange}
+        />
+      </div>
     </div>
   );
 };
