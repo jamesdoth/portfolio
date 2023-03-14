141de02834e556e53a31ee5eed358c414054e82e
jamesdoth <jamesdoth@gmail.com>
2023-03-14 11:14:55 -0700

    added form->enter->submit on todolist

--- src/content/TodoList/ToDoList.tsx
+++ src/content/TodoList/ToDoList.tsx
@@ -22,6 +22,11 @@ const TodoList = () => {
     setInputValue('');
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleAddTodo();
+  };
+
   const handleDeleteTodo = (id: number) => {
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
@@ -30,29 +35,31 @@ const TodoList = () => {
   return (
     <div className='max-w-md mx-auto'>
       <h1 className='text-gray-200 text-2xl font-medium mb-4'>My Todo List</h1>
-      <div className='flex mb-4'>
-        <input
-          type='text'
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          className='form-input w-full rounded-lg text-left pl-2'
-          placeholder='Add a new todo'
-        />
-        <button
-          onClick={handleAddTodo}
-          className='ml-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition'
-        >
-          Add
-        </button>
-      </div>
-      {todos.map((todo) => (
-        <Todo
-          key={todo.id}
-          title={todo.title}
-          id={todo.id}
-          onDelete={handleDeleteTodo}
-        />
-      ))}
+      <form onSubmit={handleSubmit}>
+        <div className='flex mb-4'>
+          <input
+            type='text'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className='form-input w-full rounded-lg text-left pl-2'
+            placeholder='Add a new todo'
+          />
+          <button
+            onClick={handleAddTodo}
+            className='ml-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition'
+          >
+            Add
+          </button>
+        </div>
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            title={todo.title}
+            id={todo.id}
+            onDelete={handleDeleteTodo}
+          />
+        ))}
+      </form>
     </div>
   );
 };
