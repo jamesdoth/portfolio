0d0267d28cdfaae5adb47ac4e0c92592b5f46835
jamesdoth <jamesdoth@gmail.com>
2023-03-22 10:45:04 -0700

    navlink

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,21 @@ interface NavLinkProps {
   label: string;
 }
 
+const NavLink: React.FC<NavLinkProps> = ({ href, label }) => {
+  return (
+    <a
+      href={href}
+      className='text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white transition-colors'
+      onClick={(e) => {
+        e.preventDefault();
+        document.querySelector(href)?.scrollIntoView({ behavior: 'smooth' });
+      }}
+    >
+      {label}
+    </a>
+  );
+};
+
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
